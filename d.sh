a3809b062d83c513145c77d9753a6f5f9c815782
The Black Paper <52263806+7870a@users.noreply.github.com>
2020-04-21 22:10:19 +0800

    Update and rename d.sh to delete.sh

--- d.sh
+++
@@ -1,4 +0,0 @@
-rm -rf Defce-Web-Tools
-echo loading
-sleep 1
-echo 10%
---
+++ delete.sh
@@ -0,0 +1,5 @@
+cd
+rm -rf Defce-Web-Tools
+git clone https://github.com/7870a/Deface-Web-Tools
+clear
+echo ketik (cd) agar proses berhasil
