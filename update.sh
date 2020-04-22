349aa5994f204dcb0caa3c41f754ce39bf55ad52
The Black Paper <52263806+7870a@users.noreply.github.com>
2020-04-22 08:45:20 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,3 +1,18 @@
+echo Loading
+sleep 2.5;
+echo 15%
+sleep 0.9;
+echo 20%
+sleep 1.9;
+echo 39%
+sleep 1.7;
+echo 60%
+sleep 3.9;
+echo 79%
+sleep 2.5;
+echo 99%
+sleep 2.9
+clear
 cd
 rm -rf Deface-Web-Tools
 git clone https://github.com/7870a/Deface-Web-Tools
