68e13236087270dbbacef0370bda9be2286179a7
The Black Paper <52263806+7870a@users.noreply.github.com>
2020-04-22 08:38:31 +0800

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,2 +1,17 @@
+echo Loading
+sleep 2.5;
+echo 10%
+sleep 0.9;
+echo 25%
+sleep 1.9;
+echo 27%
+sleep 1.7;
+echo 40%
+sleep 3.9;
+echo 79%
+sleep 2.5;
+echo 99%
+sleep 2.9
+clear
 cd
 rm -rf Deface-Web-Tools
