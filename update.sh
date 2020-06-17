106ec71b2e942b85d6e367366a61e9881acb6413
The Black Paper <52263806+7870a@users.noreply.github.com>
2020-06-17 11:46:31 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,18 +1,19 @@
-echo Loading
+echo Loading | lolcat
 sleep 2.5;
-echo 15%
+echo 15% | lolcat
 sleep 0.9;
-echo 20%
+echo 20% | lolcat
 sleep 1.9;
-echo 39%
+echo 39% | lolcat
 sleep 1.7;
-echo 60%
+echo 60% | lolcat
 sleep 3.9;
-echo 79%
+echo 79% | lolcat
 sleep 2.5;
-echo 99%
+echo 99% | lolcat
 sleep 2.9
 clear
+echo Mengupdate | lolcat
 cd
 rm -rf Deface-Web-Tools
 git clone https://github.com/7870a/Deface-Web-Tools
