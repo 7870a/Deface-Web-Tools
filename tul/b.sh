9604306b51c086a07cbc821ad45471a232e4bec9
The Black Paper <52263806+7870a@users.noreply.github.com>
2020-07-14 08:57:43 +0800

    Update b.sh

--- tul/b.sh
+++ tul/b.sh
@@ -8,20 +8,25 @@ white="\033[39;1m"
 
 clear
 
-echo Masukan Target
-echo contoh target
+echo $green
+echo Simpan SC Di Memory Internal Di Luar Folder
+echo Contoh Target:
 echo $red
-echo "http://colourfactory.co.za"
-echo $cyan
+echo "http://colourfactory.co.za"          
+echo $cyan 
 echo "http://chillibitez.co.za"
 echo $red
 echo "http://centraltech.co.za"
 echo $cyan
 echo "http://windmillsandporcupines.co.za"
 echo $red
-echo "http://mpark.co.za new"
+echo "http://mpark.co.za"
 echo $cyan
 echo "http://ratia.co.za "
+echo $red
+echo "http://itsengineering.co.za new"
+echo $cyan
+echo "http://uzingela.co.za new"
 echo $green
 read -p "Target=>"  target
 echo loading......
