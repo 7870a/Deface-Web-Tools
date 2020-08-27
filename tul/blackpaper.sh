bf1e14e698605414c1c294edd088fbe21eb34e24
The Black Paper <52263806+7870a@users.noreply.github.com>
2020-08-27 13:48:11 +0800

    Update blackpaper.sh

--- tul/blackpaper.sh
+++ tul/blackpaper.sh
@@ -1,4 +1,30 @@
+green="\033[32;1m"
+yellow="\033[33;1m"
+indigo="\033[34;1m"
+red="\033[35;1m"
+purple="\033[37;1m"
+cyan="\033[36;1m"
+white="\033[39;1m"
+
 clear
+echo  $green
+echo "The black paper.............";
+sleep 0.9;
+clear
+echo
+echo
+echo
+echo
+echo $cyan
+figlet D.W.T | lolcat
+echo ------------------------------
+echo Nama Tools:Deface-Web-Tools Premium
+echo Team:The Black Paper Indonesia
+echo Tools Created By:7870a 
+echo github:https://github.com/7870a
+echo Versi:7.0
+echo ------------------------------
+sleep 2
 cd tul
 clear
 sh bp.sh
